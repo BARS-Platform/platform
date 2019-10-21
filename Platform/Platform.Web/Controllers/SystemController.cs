205e8975c030a0f5c81ac248b8ecc8d0f3760a99
d4n0n_myself <danon.sibaev@gmail.com>
2019-10-21 13:02:59 +0300

    Добавлена обработка ошибок для приложений с Environment != Development

---
+++ Platform/Platform.Web/Controllers/SystemController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Platform.Web.Controllers
+{
+	[Route("[controller]/[action]")]
+	public class SystemController : Controller
+	{
+		[HttpGet]
+		public IActionResult Error()
+		{
+			var error = HttpContext
+				.Features
+				.Get<IExceptionHandlerPathFeature>();
+			return Conflict($"Error occured on request {error.Path} : {error.Error.Message}");
+		}
+	}
+}
