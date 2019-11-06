0d7cb70c0f706b0fd3134d8d9afb959965a63617
d4n0n_myself <danon.sibaev@gmail.com>
2019-11-06 15:41:01 +0300

    Убрал лишний пробел

--- Platform/Platform.Fatabase/IRepository.fs
+++ Platform/Platform.Fatabase/IRepository.fs
@@ -7,7 +7,7 @@ open Platform.Fodels.Interfaces
 type IRepository<'T when 'T :> IPlatformModel> =
     interface
         abstract Create: 'T -> 'T
-         abstract Delete: 'T -> bool
+        abstract Delete: 'T -> bool
         abstract Get: (int) -> 'T 
         abstract FindByPredicate: Expression<Func<'T, bool>> -> 'T
         abstract Update: 'T -> 'T
