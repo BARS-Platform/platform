ad0c268ee51a5e3637f7006356c08268a47cc950
Данэль Сибаев <danon.sibaev@gmail.com>
2019-11-23 14:35:47 +0300

    ЕЩЕ ЛИШНЕЕ

--- Platform/Platform.Services/Helpers/IncludeHelper.cs
+++ Platform/Platform.Services/Helpers/IncludeHelper.cs
@@ -57,34 +57,6 @@ namespace Platform.Services.Helpers
 			return (IQueryable<T>) operationHealth;
 		}
 
-		private static object lanvbmsd<T>(Type curElemType, string parent, Type parentType) where T : class, IAddressElement
-		{
-			ParameterExpression pe = Expression.Parameter(curElemType, "source");
-			Expression lastMember = pe;
-			MemberExpression member = Expression.Property(lastMember, parent);
-
-			var expressionLambdaMethod = typeof(Expression).GetMethods()
-				.Single(methodInfo =>
-				{
-					if (!methodInfo.IsGenericMethod)
-						return false;
-					var parameters = methodInfo.GetParameters();
-					if (parameters.Length != 2)
-						return false;
-					if (parameters[0].ParameterType != typeof(Expression))
-						return false;
-					if (parameters[1].ParameterType != typeof(ParameterExpression[]))
-						return false;
-					return true;
-				});
-
-			var genericFunc = typeof(Func<,>).MakeGenericType(curElemType, parentType);
-			var genericMethod = expressionLambdaMethod.MakeGenericMethod(genericFunc);
-
-			var lambda = genericMethod.Invoke(null, new object[] {member, new[] {pe}});
-			return lambda;
-		}
-
 		private static object MakeFuncLambdaExpressions<T>(Type curElemType, string parent, Type parentType)
 			where T : class, IAddressElement
 		{
