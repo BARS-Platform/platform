2aac4c3114eb215788eebd94cd31b9a183108dc5
Halloneo <petrushenko_artu@mail.ru>
2019-11-23 12:04:51 +0300

    добавлен контроллер на Роли

---
+++ Platform/Platform.Web/Controllers/RoleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Platform.Fatabase;
+using Platform.Fodels.Entities;
+using Platform.Fodels.Models;
+using Platform.Services.Services;
+
+namespace Platform.Web.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class RoleController : Controller
+    {
+        private readonly IRepository _repository;
+        
+        public RoleController(IRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        /// <summary>
+        /// Метод для получения ролей.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Role> GetAll()
+        {
+            var roles = _repository.FindAllByPredicate<Role>(x => true);
+            return roles;
+        }
+    }
+}
