42cf9465446f33f344ca932d378add1635a39321
Данэль Сибаев <danon.sibaev@gmail.com>
2019-11-23 14:24:23 +0300

    rocketScience

--- Platform/Platform.Services/Helpers/IncludeHelper.cs
+++ Platform/Platform.Services/Helpers/IncludeHelper.cs
@@ -2,7 +2,7 @@ using System;
 using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
+using Platform.Fatabase;
 using Platform.Fodels.Enums;
 using Platform.Fodels.Models.Address;
 
@@ -12,37 +12,98 @@ namespace Platform.Services.Helpers
 	{
 		public static IQueryable<T> IncludeAllAddressItems<T>(this IQueryable<T> source) where T : class, IAddressElement
 		{
+			using (var applicationDbContext = new ApplicationDbContext())
+			{
+				applicationDbContext.Apartments.Include(x => x.House).ThenInclude(x => x.Street).ThenInclude(x=>x.City);
+			}
 			var addressTypes = TypeHelper.GetTypes(typeof(IAddressElement));
 			var curElemType = source.ElementType;
+			var initialQueryType = source.ElementType;
 			var first = true;
-			var operationHealth = (IIncludableQueryable<T,T>)source;
+			object operationHealth = null;
 			while (curElemType != null && curElemType != typeof(Country))
 			{
 				var addressItem = Enum.Parse<AddressItem>(curElemType.Name);
 				var parent = addressItem.GetParentPropertyName();
 				if (parent == null)
-					return operationHealth;
+					return (IQueryable<T>) operationHealth ?? source;
+				var parentType = addressTypes.FirstOrDefault(x => x.Name == parent);
 				if (first)
 				{
-					ParameterExpression pe = Expression.Parameter(typeof(T), "source");
+					ParameterExpression pe = Expression.Parameter(curElemType, "source");
 					Expression lastMember = pe;
-					MemberExpression member = Expression.Property(lastMember, "Country");
-					Expression<Func<T, T>> lambda = Expression.Lambda<Func<T, T>>(member);
-					operationHealth = source.Include(lambda);
+					MemberExpression member = Expression.Property(lastMember, parent);
+
+					var expressionLambdaMethod = typeof(Expression).GetMethods()
+						.Single(methodInfo =>
+						{
+							if (!methodInfo.IsGenericMethod)
+								return false;
+							var parameters = methodInfo.GetParameters();
+							if (parameters.Length != 2)
+								return false;
+							if (parameters[0].ParameterType != typeof(Expression))
+								return false;
+							if (parameters[1].ParameterType != typeof(ParameterExpression[]))
+								return false;
+							return true;
+						});
+
+					var genericFunc = typeof(Func<,>).MakeGenericType(curElemType, parentType);
+					var genericMethod = expressionLambdaMethod.MakeGenericMethod(genericFunc);
+
+					var lambda = genericMethod.Invoke(null, new object[] {member, new[] {pe}});
+
+					var include = typeof(EntityFrameworkQueryableExtensions)
+						.GetMethods()
+						.Single(methodInfo =>
+							methodInfo.Name == "Include" &&
+							methodInfo.GetParameters()[1].ParameterType != typeof(string))
+						.MakeGenericMethod(curElemType, parentType);
+
+					operationHealth = include.Invoke(null, new[] {source, lambda});
+
 					first = false;
 				}
 				else
 				{
-					ParameterExpression pe = Expression.Parameter(typeof(T), "source");
+					ParameterExpression pe = Expression.Parameter(curElemType, "source");
 					Expression lastMember = pe;
-					MemberExpression member = Expression.Property(lastMember, "Country");
-					Expression<Func<T, T>> lambda = Expression.Lambda<Func<T, T>>(member);
-					operationHealth = operationHealth.ThenInclude(lambda);
-				}
-				curElemType = addressTypes.FirstOrDefault(x => x.Name == parent);
-			}
+					MemberExpression member = Expression.Property(lastMember, parent);
 
-			return operationHealth;
+					var expressionLambdaMethod = typeof(Expression).GetMethods()
+						.Single(methodInfo =>
+						{
+							if (!methodInfo.IsGenericMethod)
+								return false;
+							var parameters = methodInfo.GetParameters();
+							if (parameters.Length != 2)
+								return false;
+							if (parameters[0].ParameterType != typeof(Expression))
+								return false;
+							if (parameters[1].ParameterType != typeof(ParameterExpression[]))
+								return false;
+							return true;
+						});
+
+					var genericFunc = typeof(Func<,>).MakeGenericType(curElemType, parentType);
+					var genericMethod = expressionLambdaMethod.MakeGenericMethod(genericFunc);
+
+					var lambda = genericMethod.Invoke(null, new object[] {member, new[] {pe}});
+
+					var include = typeof(EntityFrameworkQueryableExtensions)
+						.GetMethods()
+						.Single(methodInfo =>
+							methodInfo.Name == "ThenInclude"
+							&& !methodInfo.GetParameters()[0].ParameterType.GenericTypeArguments[1].IsGenericType);
+					var includeGeneric = include.MakeGenericMethod(initialQueryType, curElemType, parentType);
+					operationHealth = includeGeneric.Invoke(null, new[] {operationHealth, lambda});
+				}
+
+				curElemType = parentType;
+			}
+ 
+			return (IQueryable<T>) operationHealth;
 		}
 	}
 }
