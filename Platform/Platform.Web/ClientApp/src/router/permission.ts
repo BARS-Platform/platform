51196ebcc31a46ee102c54b90191f076ca6f5bc4
Helmoz <helmo.designer1@gmail.com>
2019-11-29 21:23:47 +0300

    Убран отладочный код

--- Platform/Platform.Web/ClientApp/src/router/permission.ts
+++ Platform/Platform.Web/ClientApp/src/router/permission.ts
@@ -3,12 +3,9 @@ import * as permissionHelper from './helpers/permissionHelper'
 
 export default async (to: any, from: any, next: any) => {
   let check = await permissionHelper.Check(to.params.name)
-  console.log('test')
   if (check) {
-    console.log(check)
     next()
   } else {
-    console.log(check)
     next('/')
     Notify.create({
       message: 'Доступ запрещен',
