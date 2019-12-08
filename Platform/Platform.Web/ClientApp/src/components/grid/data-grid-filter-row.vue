489631fd57b74a82844459d31a853a10ad56139c
Helmoz <helmo.designer1@gmail.com>
2019-12-08 15:26:07 +0300

    Вынес компонент меню фильтра

--- Platform/Platform.Web/ClientApp/src/components/grid/data-grid-filter-row.vue
+++ Platform/Platform.Web/ClientApp/src/components/grid/data-grid-filter-row.vue
@@ -2,38 +2,9 @@
   <q-tr :props="props" style="height: 15px">
     <q-th :key="column.name" v-for="column in props.cols">
       <div class="row items-center">
-        <q-input class="col-11" v-if="isRegularColumn(column.name)" borderless :value="getValue(column.name)" readonly dense />
+        <q-input class="col-11" v-if="isRegularColumn(column.name)" borderless :value="getFilter(column.name)" readonly dense />
         <div class="col-1 flex justify-end">
-          <q-btn v-if="isRegularColumn(column.name)" icon="fas fa-filter" size="xs" flat round>
-            <q-menu self="top right">
-              <div class="no-wrap q-pa-md">
-                <div class="row">
-                  <q-select
-                    v-if="column.type === 'integer'"
-                    @input="setOperator(column.name, $event)"
-                    options-dense
-                    class="col-2"
-                    dense
-                    :value="getOperatorValue(column.name)"
-                    :options="options"
-                  />
-                  <q-input
-                    class="col q-ml-xs"
-                    :mask="column.type === 'integer' ? '###########################' : ''"
-                    :value="getValue(column.name)"
-                    :label="column.label"
-                    dense
-                    style="width: 270px"
-                    @input="setFilter(column, $event)"
-                  />
-                </div>
-                <div class="flex justify-end items-center q-mt-md bg-grey-2">
-                  <q-btn icon="done" @click="onRequest('filter')" size="sm" flat round />
-                  <q-btn icon="delete" @click="clearFilter(column.name)" size="sm" flat round />
-                </div>
-              </div>
-            </q-menu>
-          </q-btn>
+          <data-grid-filter-menu :applyFilter="onRequest" :filters.sync="localFilters" :column="column" />
         </div>
       </div>
     </q-th>
@@ -44,89 +15,42 @@
 import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
 
 import * as grid from '@/utils/grid'
+
 import { isActionColumn } from '@/utils/grid'
 import { Filtration } from '@/models/data/filtration'
 import { Column } from '@/models/grid/column'
 
-@Component
+import DataGridFilterMenu from '@/components/grid/data-grid-filter-menu.vue'
+
+@Component({
+  components: {
+    DataGridFilterMenu
+  }
+})
 export default class DataGridFilterRow extends Vue {
   @Prop() props!: any
   @Prop() filters!: Filtration[]
   @Prop() onRequest!: Function
 
-  options = ['=', '!=', '<', '<=', '>', '>=']
-
-  fils: Filtration[] = this.filters
+  localFilters: Filtration[] = []
 
   @Watch('filters', { immediate: true, deep: true })
-  async onFilterChange(newVal: any) {
-    this.fils = newVal
+  async onFiltersChange(filters: Filtration[]) {
+    this.localFilters = filters
   }
 
-  getValue(columnName: string) {
-    let fil = this.fils.find(x => x.columnName == columnName)
-    if (fil) {
-      if (fil.columnOperator) return fil.columnOperator + fil.columnValue
-      else return fil.columnValue
-    } else {
-      return ''
-    }
+  @Watch('localFilters', { immediate: true, deep: true })
+  async onLocalFiltersChange(filters: Filtration[]) {
+    this.$emit('update:filters', this.localFilters)
   }
 
-  getOperatorValue(columnName: string) {
-    let fil = this.fils.find(x => x.columnName == columnName)
-    if (fil) {
-      return fil.columnOperator
-    } else {
-      return '='
-    }
-  }
-
-  clearFilter(columnName: string) {
-    let fil = this.fils.find(x => x.columnName == columnName)
-    if (fil) {
-      let index = this.fils.indexOf(fil)
-      this.fils.splice(index, 1)
-    }
-  }
-
-  setOperator(columnName: string, value: string) {
-    let fil = this.fils.find(x => x.columnName == columnName)
-    if (fil) {
-      fil.columnOperator = value
-    } else {
-      this.fils.push({
-        columnName: columnName,
-        columnOperator: value,
-        columnValue: ''
-      })
-    }
-    this.$emit('update:filters', this.fils)
-  }
-
-  setFilter(column: Column, value: string) {
-    let fil = this.fils.find(x => x.columnName == column.name)
-    if (fil) {
-      fil.columnValue = value
-    } else {
-      let fil: Filtration = {
-        columnName: column.name,
-        columnValue: value
-      }
-      if (column.type === 'integer') {
-        fil.columnOperator = '='
-      }
-      this.fils.push(fil)
-    }
-    this.$emit('update:filters', this.fils)
+  getFilter(columnName: string) {
+    let filter = this.localFilters.find(x => x.columnName == columnName)
+    return filter ? (filter.columnOperator ? filter.columnOperator + filter.columnValue : filter.columnValue) : ''
   }
 
   isRegularColumn(columnName: string) {
     return !grid.isActionColumn(columnName)
-  }
-
-  getRegularColumns(columns: { name: string }[]) {
-    return grid.getRegularColumns(columns)
   }
 }
 </script>
